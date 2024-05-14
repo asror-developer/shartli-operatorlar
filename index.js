0d27ebca560ef805ed672e61fa0689670e368a56
asror-developer <asroreshnazarov8@gmail.com>
2024-05-14 17:58:51 +0500

    shartli operatorlar

---
+++ index.js
@@ -0,0 +1,374 @@
+// if1. 
+// let a=7;
+// if (a>0) {
+//     console.log(a+=1);
+// }
+// else{
+//     console.log(a-=2);
+// }
+
+// if2. 
+// let a=10
+// let b=-7
+// let c=-4
+// if (a>0&&b>0&&c>0) {
+//     console.log("sonlar musbat");
+// }
+    
+//  else if ((a<0&&b>0&&c>0)||(b<0&&a>0&&c>0)||(c<0&&a>0&&b>0)) {
+//     console.log("1 ta manfiy son bor");
+    
+// } else if ((a<0&&b<0&&c>0)||(a>0&&b<0&&c<0)||(a<0&&b<0&&c>0)) {
+//     console.log("2 ta manfiy son");
+
+    
+// }else{
+//     console.log(" sonlar manfiy");
+// }
+    
+
+
+// if3. 
+// let a=7
+// let b=11
+// let c=4
+// if (a<b&&b<c) {console.log(a, b, c);
+    
+// } else if (b<a&&a<c) {
+//     console.log(b, a, c);
+    
+// } else if (a<c&&c<b) {
+//     console.log(a,c,b);
+// } else if (b<c&&c<a) {
+//     console.log(b,c,a);
+// } else if (c<b&&b<a) {
+//     console.log(c,b,a);
+    
+// } else{
+//     console.log(c,a,b);
+// }
+
+// if4. 111111111111111111
+
+
+// if5. x haqiqiy soni berilgan. Quyidagi funksiya hisoblansin.
+// let x=17
+
+// if( x < -2 || x > 2 )
+// {console.log(2 * x );}
+// else{  console.log(-3 * x);}
+
+// if6. 
+// let a=12
+// if (a%2===0&&a>0) {
+//     console.log("musbat juft son");
+    
+// } else if (a%2!=0&&a>0) {
+//     console.log("musbat toq son");
+    
+// }else  if (a%2===0&&a<0) {
+//     console.log("manfiy juft son");
+    
+// } else if (a%2!=0&&a<0) {
+// console.log("manfiy toq son");    
+// } else{
+//     console.log("0");
+    
+// }
+
+// Case1. 
+// 
+
+// const kun=5
+
+// switch (kun) {
+//     case 1: 
+//      console.log("dushanba"); break;
+//     case 2: 
+//      console.log("seshanba"); break;
+//     case 3:
+//      console.log("chorshanba"); break;
+//     case 4: 
+//      console.log("payshanba"); break;
+//     case 5: 
+//      console.log("juma"); break;
+//     case 6:  
+//      console.log("shanba"); break;
+//     case 7: 
+//      console.log("yakshanba"); break;
+//     default:
+//         console.log("hafta kuni 7 kun");
+//         break;
+// }
+
+// Case2. 
+// let a=10
+// let b=7
+
+// switch (ishora ) {
+//     case "+":
+//      console.log(a+b);break;
+//     case "-":
+//      console.log(a-b);break;
+//     case "*":
+//      console.log(a*b);break;
+//     case "/":
+//      console.log(a/b);break;
+        
+
+
+//     default:
+//         console.log("ishorani xato kiritdingiz");
+//         break;
+// }
+
+// if1. 
+
+// a=20
+// if (a>0) {
+//     console.log(a+=1);
+// } else {
+//     console.log(a);
+    
+// }
+
+// if2. 
+
+// a=14
+// if (a>0) {
+//     console.log(a+=1);
+// } else if (a<0) {
+//     console.log(a-=2);
+    
+// } else{
+//     console.log(10);
+    
+// }
+// if3.
+// let a=11
+// let b=7
+// let c=3
+// if (a>0&&b>0&&c>0) {
+//     console.log("sonlar musbat");
+
+    
+// } else if ((a<0&&b>0&&c>0)||(b<0&&a>0&&c>0)||(c<0&&a>0&&b>0)) {
+//     console.log("1 ta manfiy son bor");
+    
+// } else if ((a<0&&b<0&&c>0)||(a>0&&b<0&&c<0)||(a<0&&b<0&&c>0)) {
+//     console.log("2 ta manfiy son");
+
+    
+// }else{
+//     console.log(" sonlar manfiy");
+// }
+
+// if4. 
+// a=6
+// b=17
+// if (a>b) {
+//     console.log(a);
+    
+// } else {
+//     console.log(b);
+    
+// }
+
+// if5. 11111111111111111111111111111111
+
+
+// if6.
+// a=9
+// b=15
+// if (a>b) {
+//     console.log(a,b);
+    
+// } else {
+//     console.log(b,a);
+    
+// }
+
+// if7. 
+
+// a=21
+// b=8
+// if (a>b) {
+//     a-=9;
+//     b+=9;
+//     console.log(a,b);
+// }
+// else{
+//     console.log("xatolik bor");
+// }
+// console.log(a,b);
+
+
+// if8. 
+// a=12
+// b=7
+// if (a!=b) {
+//     console.log(a+b);
+    
+// }
+// else{
+//     console.log(a-b);
+// }
+// console.log(a,b);
+
+// if9.
+
+// a=16
+// b=7
+// if (a!=b&&a>b) {
+//     console.log(a);
+    
+// } else if (a!=b&&b>a) {
+//     console.log(b);
+    
+// } else{
+//     console.log("0");
+    
+// }
+
+
+// if10
+
+// let x=44
+
+// if(x>0){
+//     console.log(x+=1);
+// }
+// else if (x<0){
+//     console.log(x);
+// }
+
+// if11
+
+// let x=11
+
+// if(x>0){
+//     console.log(x+=1);
+// }                                   
+// else if(x<0){
+//     console.log(x-=2);
+// }
+// else if(x==0){
+//     x=10;
+//     console.log(x);
+// }
+
+
+// if12
+
+// let x=2
+// let y=13
+
+// if(x>y){
+//     console.log("katta son:",x);
+// }
+// else if(y>x){
+//     console.log("katta son:",y);
+// }
+
+
+// if13
+
+// let x=13
+// let y=32
+
+// if(x>y){
+//     console.log("2-son:",y);
+// }
+// else if(x<y){
+//     console.log("1-son:",x);
+// }
+
+
+
+// if14
+
+// let A=32
+// let B=4
+// let C=51
+
+
+// if(A<B && A<C && B<C ||A>B && A>C && B>C){
+//     console.log(2*A, 2*B, 2*C);
+// }
+// else{
+//     console.log(-A, -B, -C);
+// }
+
+
+// if16
+
+// let x=84
+
+// if(x<=0){
+//     console.log(-x);
+// }
+// else if(0<x<2){
+//     console.log(x**2);
+// }
+// else if(x>=2){
+//     x==4;
+//     console.log(x);
+// }
+
+// if17
+
+// let y=9
+// if( y%4==0 && y<=400 ){
+//     console.log("kabisa:",y);
+// }
+// else if(y%400==0 && y>=400){
+//     console.log("kabisa:",y);
+// }
+// else if(y%4!==0 && y<=400){
+//     console.log("kabisa emas:",y);
+// }
+// else if(y%400!==0 && y>=400){
+//     console.log("kabisa emas:",y);
+// }
+
+    // if18
+
+//     let x=71
+
+//     if(0<=x<10 && x%2==0){
+//         console.log("bir xonali juft son:",x);
+//  }
+//     else if(0<=x<10 && x%2!==0){
+//         console.log("bir xonali toq son",x);
+// }
+//     else if(10<=x<100 && x%2==0){
+//         console.log("ikki xonali juft son:",x);
+// }
+//     else if(10<=x<100 && x%2!==0){
+//         console.log("ikki xonali toq son:",x);
+// }
+//     else if(100<=x<1000 && x%2==0){
+//         console.log("uch xonali juft son:",x);
+// }
+//     else if(100<=x<1000 && x%2!==0){
+//         console.log("uch xonali toq   son:",x);
+// }
+
+
+let museum = 120;
+let hotel = 250;
+let ticket = 500;
+let dollar = 9433.34;
+let euro = 10354.03;
+let addition = dollar * (museum + hotel + ticket)
+let result = euro * (museum + hotel + ticket)
+let money = prompt("Enter your money in dollars:")
+let sum = prompt("Enter your money in evro:")
+if (money>addition) {
+    console.log("Welcome");
+} else if (sum>result) {
+    console.log("Welcome");
+}
+else{
+    console.log("Please wait");
+}
